efd06407de313fef0094948ce7993b98ce60b2b8
Cai.Cai <yangcai.email@gmail.com>
2022-03-24 17:38:15 +0800

    limit update

--- limit/core/limit/periodlimit.go
+++ limit/core/limit/periodlimit.go
@@ -8,6 +8,37 @@ import (
 	"github.com/zeromicro/go-zero/core/stores/redis"
 )
 
+// https://juejin.cn/post/7051406419823689765
+// https://juejin.cn/post/6895928148521648141
+
+/*
+
+-- KYES[1]:限流器key
+-- ARGV[1]:qos,单位时间内最多请求次数
+-- ARGV[2]:单位限流窗口时间
+-- 请求最大次数,等于p.quota
+local limit = tonumber(ARGV[1])
+-- 窗口即一个单位限流周期,这里用过期模拟窗口效果,等于p.permit
+local window = tonumber(ARGV[2])
+-- 请求次数+1,获取请求总数
+local current = redis.call("INCRBY",KYES[1],1)
+-- 如果是第一次请求,则设置过期时间并返回 成功
+if current == 1 then
+  redis.call("expire",KYES[1],window)
+  return 1
+-- 如果当前请求数量小于limit则返回 成功
+elseif current < limit then
+  return 1
+-- 如果当前请求数量==limit则返回 最后一次请求
+elseif current == limit then
+  return 2
+-- 请求数量>limit则返回 失败
+else
+  return 0
+end
+
+*/
+
 // to be compatible with aliyun redis, we cannot use `local key = KEYS[1]` to reuse the key
 const periodScript = `local limit = tonumber(ARGV[1])
 local window = tonumber(ARGV[2])
@@ -43,15 +74,20 @@ var ErrUnknownCode = errors.New("unknown status code")
 
 type (
 	// PeriodOption defines the method to customize a PeriodLimit.
+	// go中常见的option参数模式
+	// 如果参数非常多，推荐使用此模式来设置参数
 	PeriodOption func(l *PeriodLimit)
 
 	// A PeriodLimit is used to limit requests during a period of time.
+	// 固定时间窗口限流器
 	PeriodLimit struct {
-		period     int
-		quota      int
-		limitStore *redis.Redis
-		keyPrefix  string
-		align      bool
+		period     int          // 窗口大小，单位s
+		quota      int          // 请求上限
+		limitStore *redis.Redis // 存储
+		keyPrefix  string       // key前缀
+		// 线性限流，开启此选项后可以实现周期性的限流
+		// 比如quota=5时，quota实际值可能会是5.4.3.2.1呈现出周期性变化
+		align bool
 	}
 )
 
@@ -74,7 +110,14 @@ func NewPeriodLimit(period, quota int, limitStore *redis.Redis, keyPrefix string
 }
 
 // Take requests a permit, it returns the permit state.
+// 执行限流
+// 注意一下返回值：
+// 0：表示错误，比如可能是redis故障、过载
+// 1：允许
+// 2：允许但是当前窗口内已到达上限
+// 3：拒绝
 func (h *PeriodLimit) Take(key string) (int, error) {
+	// 执行lua脚本
 	resp, err := h.limitStore.Eval(periodScript, []string{h.keyPrefix + key}, []string{
 		strconv.Itoa(h.quota),
 		strconv.Itoa(h.calcExpireSeconds()),
@@ -100,6 +143,10 @@ func (h *PeriodLimit) Take(key string) (int, error) {
 	}
 }
 
+// 计算过期时间也就是窗口时间大小
+// 如果align==true
+// 线性限流，开启此选项后可以实现周期性的限流
+// 比如quota=5时，quota实际值可能会是5.4.3.2.1呈现出周期性变化
 func (h *PeriodLimit) calcExpireSeconds() int {
 	if h.align {
 		now := time.Now()
--- limit/core/limit/tokenlimit.go
+++ limit/core/limit/tokenlimit.go
@@ -12,6 +12,53 @@ import (
 	xrate "golang.org/x/time/rate"
 )
 
+// https://juejin.cn/post/6898915539851149325
+
+/*
+
+-- 返回是否可以活获得预期的token
+
+local rate = tonumber(ARGV[1])
+local capacity = tonumber(ARGV[2])
+local now = tonumber(ARGV[3])
+local requested = tonumber(ARGV[4])
+
+-- fill_time：需要填满 token_bucket 需要多久
+local fill_time = capacity/rate
+-- 将填充时间向下取整
+local ttl = math.floor(fill_time*2)
+
+-- 获取目前 token_bucket 中剩余 token 数
+-- 如果是第一次进入，则设置 token_bucket 数量为 令牌桶最大值
+local last_tokens = tonumber(redis.call("get", KEYS[1]))
+if last_tokens == nil then
+    last_tokens = capacity
+end
+
+-- 上一次更新 token_bucket 的时间
+local last_refreshed = tonumber(redis.call("get", KEYS[2]))
+if last_refreshed == nil then
+    last_refreshed = 0
+end
+
+local delta = math.max(0, now-last_refreshed)
+-- 通过当前时间与上一次更新时间的跨度，以及生产token的速率，计算出新的token数
+-- 如果超过 max_burst，多余生产的token会被丢弃
+local filled_tokens = math.min(capacity, last_tokens+(delta*rate))
+local allowed = filled_tokens >= requested
+local new_tokens = filled_tokens
+if allowed then
+    new_tokens = filled_tokens - requested
+end
+
+-- 更新新的token数，以及更新时间
+redis.call("setex", KEYS[1], ttl, new_tokens)
+redis.call("setex", KEYS[2], ttl, now)
+
+return allowed
+
+*/
+
 const (
 	// to be compatible with aliyun redis, we cannot use `local key = KEYS[1]` to reuse the key
 	// KEYS[1] as tokens_key
